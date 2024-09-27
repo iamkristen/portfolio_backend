fa052f7885b70525c02db0b6e972a2d78414b883
Kristen <iamkristen220@gmail.com>
2024-09-27 15:47:46 +0100

    testing

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
+  res.end("Hello, Node.js is working!\n");
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running...");
+});
